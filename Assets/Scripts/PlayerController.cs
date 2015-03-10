f8b69f9ef1e80def2f23b0eec16eb53c7c00efd8
yuh25 <yuh25@live.com.au>
2015-03-10 17:43:03 +1100

    Test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+
+	void Start(){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();				
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count:" + count.ToString();
+		if(count >= 10)
+			winText.text = "You Win!";
+	}
+}
